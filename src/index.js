a282d9bf7736c7b3cfeb4712a03cf4bd41acc92f
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2025-07-14 19:47:20 +0100

    Created Basic Factory Functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
 import "./styles.css";
+
+function createBattleship(length) {
+  return {
+    length,
+    hits: 0,
+    hit() {
+      this.hits++;
+    },
+    isSunk() {
+      return this.hits >= this.size;
+    },
+  };
+}
+
+function createGameBoard() {
+  const ships = [];
+  const misses = [];
+  const board = Array(10)
+    .fill(null)
+    .map(() => Array(10).fill(null));
+
+  function placeShip(ship, startX, startY, isHorizontal = True) {}
+  function receiveAttack(x, y) {}
+  function allSunk() {
+    for (let i; i <= ships.length; i++) {
+      if (ships[i].isSunk()) {
+        continue;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  return {
+    placeShip,
+    receiveAttack,
+    allSunk,
+    get missedAttacks() {
+      return missedAttacks;
+    },
+    get board() {
+      return board;
+    },
+    get ships() {
+      return ships;
+    },
+  };
+}
